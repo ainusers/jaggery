0b17b0d03f36712fcbd283764b7e4d58673b3700
tianyong <979111986@qq.com>
2020-06-03 17:53:09 +0800

    jaggery init

---
+++ src/main/java/com/example/jaggery/JaggeryApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaggery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaggeryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaggeryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jaggery/JaggeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaggery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaggeryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
